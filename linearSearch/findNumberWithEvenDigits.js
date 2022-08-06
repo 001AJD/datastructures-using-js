d4dace4e31762bed14881303e4f1dae9f7c688db
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2022-08-06 22:56:38 +0530

    updates

---
+++ linearSearch/findNumberWithEvenDigits.js
@@ -0,0 +1,38 @@
+const findNumberWithEvenDigits = (arr) =>{
+  if(!Array.isArray(arr))
+  {
+    return 'Not an array';
+  }
+  let result = 0;
+  arr.forEach(element => {
+    if(isEvenDigits(element))
+    {
+      result += 1;
+    }
+  });
+  return result;
+};
+
+const isEvenDigits = (num) => {
+  if(countDigits(num) % 2 == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+};
+
+const countDigits = (num, sum = 0) => {
+  if(num === 0)
+  {
+    return sum;
+  }
+  else
+  {
+    sum += 1;
+    return countDigits(parseInt(num/10), sum);
+  }
+};
+export{ findNumberWithEvenDigits }
--- linearSearch/findNumberWithEvenDigits.test.js
+++ linearSearch/findNumberWithEvenDigits.test.js
@@ -1,8 +1,20 @@
 import { expect } from 'chai';
+import { findNumberWithEvenDigits } from './findNumberWithEvenDigits.js';
 
 describe('Test Suite for findNumberWithEvennumber of digits',()=>{
   it('should return number of elements having even number of digits', (done)=>{
-    
+    const input = [102,200,199,10,1];
+    const expectedResult = 1
+    const actualResult = findNumberWithEvenDigits(input);
+    expect(actualResult).to.be.equal(expectedResult);
+    done();
+  });
+
+  it('should return not an array', (done)=>{
+    const input = 'my string';
+    const expectedResult = 'Not an array';
+    const actualResult = findNumberWithEvenDigits(input);
+    expect(actualResult).to.be.equal(expectedResult);
     done();
   });
 });

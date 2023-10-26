fd9e1dc3d920495e113b984196d633383ed4c3e6
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-10-27 01:24:28 +0530

    updates

--- arrays/removeElementInPlace.js
+++ arrays/removeElementInPlace.js
@@ -1,8 +1,7 @@
 const removeElementInPlace = (nums, val) => {
-  while(nums.includes(val))
-  {
-    nums.splice(nums.indexOf(val),1);
-  }
+  let arr = nums.filter((item) => item !== val);
+  nums.splice(0,nums.length);
+  nums.push(...arr);
   return nums.length;
 };
 export { removeElementInPlace }
